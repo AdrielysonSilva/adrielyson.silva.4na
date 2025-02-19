1701779272d62801707fd8569d4881a62a1173e8
AdrielysonSilva <165275794+AdrielysonSilva@users.noreply.github.com>
2025-02-19 01:34:57 +0000

    aula2 e desafio

---
+++ desafios/desafioAula2.py
@@ -0,0 +1,16 @@
+# 1. Definir as listas x e y
+lista1 = [1, 3, 4, 5]
+lista2 = [7, 8, 9, 10]
+# 2. Calcular as médias de x e y
+mediax = sum(lista1)/5
+mediay = sum(lista2)/5
+
+# 3. Inicializar as variáveis para os somatórios
+
+# 4. Utilizar um loop para calcular:
+#    - A soma de (x_i - média_x) * (y_i - média_y)
+#    - A soma de (x_i - média_x)²
+
+# 5. Calcular beta1 e beta0 usando as fórmulas dos mínimos quadrados
+
+# 6. Imprimir os resultados
